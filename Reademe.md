323bb23625f6d095cd95c00a020735c1684278dc
OmkarSahastrabudhe <omkarsahastrabudheofficial@gmail.com>
2026-02-18 22:15:13 +0530

    Ec2 instance commit

---
+++ Reademe.md
@@ -0,0 +1,12 @@
+* Terarform 
+
+** Diffrent Blocks In Terraform 
+
+resource block 
+data block 
+variable block 
+provider block 
+module block 
+terraform block
+
+
---
+++ TerraformAwsResources/Ec2Instance.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "web_server" {
+    ami = var.ami
+    instance_type = "t2.small"
+    key_name = var.key_pair
+    vpc_security_group_ids = [data.aws_default_security_group.defaultSecurityGroup.id]
+     user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install apache2 -y
+                sudo systemctl --now enable apache2
+                EOF
+                
+
+}
+
+variable "ami" {
+    description = "default ami ubuntu"
+    default =  "ami-019715e0d74f695be"
+}
+variable "key_pair" {
+    default = "superkey" 
+  
+}
+
+data "aws_default_vpc" "defaultVpc" {
+    default = true
+
+  
+}
+data "aws_default_security_group" "defaultSecurityGroup" {
+    vpc_id = data.aws_default_vpc.defaultVpc.id
+  
+}
+
+
+    
+  
+
+
+
+  
+
+
